2fed29944b2f413b675ba86afd83b6dce4362d96
mie6996 <nguyenxuanphat060301@gmail.com>
2022-01-27 13:44:36 +0700

    .

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const port = 3005 || process.env.PORT;
+const port = process.env.PORT ||3000;
 
 app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
